ae5bcee07ecc34621768679c4ff44d84642d8ef0
alican yilmaz <alicanyilmaz101@gmail.com>
2022-05-04 23:18:50 +0300

    last state

--- EfCoreTutorial.CodeFirst/Program.cs
+++ EfCoreTutorial.CodeFirst/Program.cs
@@ -6,17 +6,18 @@ Initializer.Build();
 using (var context = new AppDbContext())
 {
 
-    context.Products.Add(new Product() { Name = "Thinking in Java" , Price = 212 , Barcode = 21 , Stock = 234});
-    context.Products.Add(new Product() { Name = "Red of the claims", Price = 212, Barcode = 21, Stock = 234 });
-    context.Products.Add(new Product() { Name = "Morgot Robie", Price = 212, Barcode = 21, Stock = 234 });
+    var product = context.Products.First(x=>x.Id == 4); // Id si 4 olan datayı getirir yoksa Exception fırlatır.
+    var product2 = context.Products.FirstOrDefault(x => x.Id == 4); // Id si 4 olan datayı getirir yoksa geriye null döndürür.
+    var product3 = context.Products.FirstOrDefault(x => x.Id == 4,new Product() { Id = 2 , Barcode = 2 , Name = "Rsa" , Price = 1 , Stock = 1}); // Id si 4 olan datayı getirir yoksa 2.parametre ile verdiğimiz objeyi(Product nesnesini döndürür.)
+    var product4 = await context.Products.SingleAsync(x => x.Id == 4);  // Id si 4 olan datayı getirir
+    var product5 = await context.Products.SingleAsync(x => x.Id > 4); // Id si 4 den büyük olan birden fazla data bulursa exception fırlatır.
+    var products6 = await context.Products.Where(x => x.Id > 10 && x.Name == "mordor" || x.Price >= 12).ToListAsync();
+    var products7 = await context.Products.FirstOrDefaultAsync(x => x.Id > 10 && x.Name == "mordor" || x.Price >= 12);
 
-    Console.WriteLine($"Context Id : {context.ContextId}");
-
-    //var products = await context.Products.AsNoTracking().ToListAsync();
-    //products.ForEach(p =>
-    //{
-    //    p.Stock += 100;
-    //});
+    var productx1 = context.Products.FirstAsync(x => x.Id == 4);
+    var productx2 = context.Products.SingleAsync(x => x.Id == 4);
+    var productx3 = context.Products.FindAsync(10,36); // normalde tek parametre veririrz oda PRIMARYKEY olmalı ama tablo yapımızda birden fazla column
+                                                       // primarykey ise o zaman bunları virgülle sıra ile veirirz ve ona göre datayı getirir.
 
     context.SaveChanges();
 }
