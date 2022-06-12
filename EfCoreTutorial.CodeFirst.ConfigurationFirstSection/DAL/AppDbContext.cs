eef69a89e6ec76cddeefe9e5e63dab64e4291860
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-06-12 11:39:42 +0300

    AppDbContext created.

---
+++ EfCoreTutorial.CodeFirst.ConfigurationFirstSection/DAL/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace EfCoreTutorial.CodeFirst.ConfigurationFirstSection.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.Entries().ToList().ForEach(e =>
+            {
+                if (e.Entity is Product product)
+                {
+                    if (e.State == EntityState.Added)
+                    {
+                        product.CreatedDate = DateTime.Now;
+                    }
+                }
+            });
+            return base.SaveChanges();
+        }
+    }
+}
