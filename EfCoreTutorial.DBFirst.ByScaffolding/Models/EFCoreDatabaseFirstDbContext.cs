e64c9cc1a1707c6e255a7926eb197013bf7284c0
Alican <Alican@DESKTOP-I5FGRM3>
2022-06-12 09:54:13 +0300

    connection string updated.

--- EfCoreTutorial.DBFirst.ByScaffolding/Models/EFCoreDatabaseFirstDbContext.cs
+++ EfCoreTutorial.DBFirst.ByScaffolding/Models/EFCoreDatabaseFirstDbContext.cs
@@ -23,7 +23,7 @@ namespace EfCoreTutorial.DBFirst.ByScaffolding.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-6RFR8RS;Initial Catalog=EFCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
