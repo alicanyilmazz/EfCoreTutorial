4577e5dc25e176d7bb97c0b3589334e02e2b9a16
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-06-12 23:39:35 +0300

    AppDbContext created.

---
+++ OnetoManyRelationShip/DAL/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnetoManyRelationShip.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.Entries().ToList().ForEach(e =>
+            {
+                if (e.Entity is Product product)
+                {
+                    if (e.State == EntityState.Added)
+                    {
+                        product.CreatedDate = DateTime.Now;
+                    }
+                }
+            });
+            return base.SaveChanges();
+        }
+    }
+}
