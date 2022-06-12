486130ce694ecdb1c6f8e7b20661fa5fcb86c51b
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-06-12 11:39:04 +0300

    removed unnecessary namespaces.

--- EfCoreTutorial.CodeFirst/DAL/AppDbContext.cs
+++ EfCoreTutorial.CodeFirst/DAL/AppDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace EfCoreTutorial.CodeFirst.DAL
 {
