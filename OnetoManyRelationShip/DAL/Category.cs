a63c13f69d338e95c630d642b72a678008cc56d3
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-06-19 14:01:48 +0300

    Category Added.

--- OnetoManyRelationShip/DAL/Category.cs
+++ OnetoManyRelationShip/DAL/Category.cs
@@ -10,5 +10,6 @@ namespace OnetoManyRelationShip.DAL
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
