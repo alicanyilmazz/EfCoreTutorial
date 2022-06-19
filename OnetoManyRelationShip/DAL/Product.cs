518bf1d4f6f498f45ca8633b67b1bb47fa51b1be
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-06-19 14:01:57 +0300

    Product Added.

--- OnetoManyRelationShip/DAL/Product.cs
+++ OnetoManyRelationShip/DAL/Product.cs
@@ -14,5 +14,6 @@ namespace OnetoManyRelationShip.DAL
         public int Stock { get; set; }
         public int Barcode { get; set; }
         public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
