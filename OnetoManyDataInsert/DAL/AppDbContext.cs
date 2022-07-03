05e3f85bb53c82e7a3c8525fae24121928af03f2
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-03 11:22:54 +0300

    AppDbContext added.

---
+++ OnetoManyDataInsert/DAL/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnetoManyDataInsert.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
