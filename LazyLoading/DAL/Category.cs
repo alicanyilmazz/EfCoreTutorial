48159cec159486862532336020ef82246b195497
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-09 22:33:02 +0300

    Category created.

---
+++ LazyLoading/DAL/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoading.DAL
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
