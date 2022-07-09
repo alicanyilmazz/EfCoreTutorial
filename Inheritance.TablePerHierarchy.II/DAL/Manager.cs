7b09e026ad06dff61f720d980481f0995880e7d9
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-09 22:30:58 +0300

    Manager created.

---
+++ Inheritance.TablePerHierarchy.II/DAL/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.TablePerHierarchy.II.DAL
+{
+    public class Manager : BasePerson
+    {
+        public int Degree { get; set; }
+    }
+}
