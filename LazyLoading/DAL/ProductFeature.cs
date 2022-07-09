82b6e8d708c9ef192cd4f3112f4a69c0bb52bec2
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-09 22:33:33 +0300

    ProductFeature created.

---
+++ LazyLoading/DAL/ProductFeature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoading.DAL
+{
+    public class ProductFeature
+    {
+        public int Id { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Color { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
