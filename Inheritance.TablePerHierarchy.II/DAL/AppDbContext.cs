47f2dac59173b07c87e1d996302d980746442484
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-09 22:30:21 +0300

    AppDbContext created.

---
+++ Inheritance.TablePerHierarchy.II/DAL/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.TablePerHierarchy.II.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<BasePerson> BasePersons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
