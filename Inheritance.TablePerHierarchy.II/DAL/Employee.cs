df2f799a8fc8a8d2096f79645f9784c77e875257
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-09 22:30:38 +0300

    Employee created.

---
+++ Inheritance.TablePerHierarchy.II/DAL/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.TablePerHierarchy.II.DAL
+{
+    public class Employee : BasePerson
+    {
+        [Precision(18, 2)]
+        public decimal Salary { get; set; }
+    }
+}
