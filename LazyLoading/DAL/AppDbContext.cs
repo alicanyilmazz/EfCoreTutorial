28a8283533eec8350c4bcd5193eca50012f93341
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-09 22:32:53 +0300

    AppDbContext created.

---
+++ LazyLoading/DAL/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoading.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeature { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.LogTo(Console.WriteLine,Microsoft.Extensions.Logging.LogLevel.Information).UseLazyLoadingProxies().UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(x => x.Products).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId);
+            modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
+// Trace
+// Debug 
+// Information --> LogTo ya Information duzeyinde olanları logla dediğimiz için Information ve uzerindekileri sadece Log layacaktır.
+// Warning
+// Error
+// Critical
