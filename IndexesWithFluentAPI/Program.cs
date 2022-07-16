89a5dabd3d89995916fe2e3e8e38b9d174ab5ad4
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-16 10:18:42 +0300

    program.cs modified.

---
+++ IndexesWithFluentAPI/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IndexesWithFluentAPI.DAL;
+
+Console.WriteLine("Hello, World!");
+
+using (var context = new AppDbContext())
+{
+    var result = context.Products.Where(x=>x.Name == "Macbook").Select(x=> new {Name = x.Name, Price = x.Price, Stock = x.Stock});
+}
