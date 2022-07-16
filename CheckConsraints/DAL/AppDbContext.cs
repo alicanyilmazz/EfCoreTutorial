53eef663cfb8cc853df8d263381fcb6383337456
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-16 09:59:35 +0300

    AppDbContext added.

---
+++ CheckConsraints/DAL/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckConsraints.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeature { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(x => x.Products).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId);
+            modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id);
+
+            // Birinci Parametre Consraint ismi , ikinci parametre sql cumleciğimiz olacaktır.
+            // Aşağıdaki Consraint ile Price alanının DiscountPrice alanından her zaman buyuk olacagını DB duzeyinde garanti altına almıs olduk.
+            modelBuilder.Entity<Product>().HasCheckConstraint("PriceDiscountCheck", "[Price]>[DiscountPrice]");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
