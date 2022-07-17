00034ab13951c762a452fa7ea882e8d8237c2691
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-17 22:10:23 +0300

    program.cs modified.

---
+++ ToSqlQueryMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using ToSqlQueryMethod.DAL;
+
+using (var context = new AppDbContext())
+{
+    if (context.Categories.FirstOrDefault() == null)
+    {
+        var category = new Category() { Name = "Electronics" };
+        category.Products.Add(new() { Name = "Macbook Pro 13 inch", Price = 123, Stock = 23, Barcode = 923, ProductFeature = new() { Color = "red", Height = 2, Width = 3 } });
+        category.Products.Add(new() { Name = "Macbook Air 13 inch", Price = 213, Stock = 22, Barcode = 965, ProductFeature = new ProductFeature() { Color = "blue", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Macbook Pro 14 inch", Price = 34, Stock = 2, Barcode = 33, ProductFeature = new ProductFeature() { Color = "gray", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Macbook Pro Max 16 inch", Price = 13, Stock = 4, Barcode = 47, ProductFeature = new ProductFeature() { Color = "pink", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Iphone 13 Pro", Price = 44, Stock = 31, Barcode = 11, ProductFeature = new ProductFeature() { Color = "orange", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Iphone 13 Max", Price = 66, Stock = 3, Barcode = 31, ProductFeature = new ProductFeature() { Color = "rose", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Apple Watch", Price = 83, Stock = 44, Barcode = 55, ProductFeature = new ProductFeature() { Color = "black", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Apple Watch SE", Price = 96, Stock = 56, Barcode = 99, ProductFeature = new ProductFeature() { Color = "seablue", Height = 1, Width = 9 } });
+        category.Products.Add(new() { Name = "Iphone SE", Price = 553, Stock = 676, Barcode = 7, ProductFeature = new ProductFeature() { Color = "darkgray", Height = 1, Width = 9 } });
+
+        await context.Categories.AddAsync(category);
+
+        await context.SaveChangesAsync();
+    }
+    //--------------------------------------------------------------------------------------------------------------------//
+
+    var products = context.ProductEssential.ToList(); //  modelBuilder.Entity<ProductEssential>().ToSqlQuery("Select Name,Price From Products"); DB ye bu sorguyu atar.
+    var products2 = context.ProductEssential.Where(x=>x.Price > 200).ToList(); // Burdaki eklediğimiz Where kosulunu
+    //  modelBuilder.Entity<ProductEssential>().ToSqlQuery("Select Name,Price From Products"); bulunan SQL cümleciğine ekler ve öyle DB ye sorgu atar , yani gidip
+    // Önce veriyi getirip sonrasında Where kosulunu calıstırmaz Where i de ToSqlQuery içerisindeki SQL içerisine ekler öyle gidip DB ye sorgu atar.
+}
