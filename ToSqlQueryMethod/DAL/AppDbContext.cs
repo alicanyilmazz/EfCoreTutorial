3beec6745b78ec89615583b4804bc4267181ef48
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-17 22:09:29 +0300

    AppDbContext added.

---
+++ ToSqlQueryMethod/DAL/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToSqlQueryMethod.DAL.Model;
+
+namespace ToSqlQueryMethod.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeature { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductEssential> ProductEssential { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information).UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(x => x.Products).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId);
+            modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id);
+
+            modelBuilder.Entity<ProductEssential>().HasNoKey(); // Bu bir Custom sorgu oldugundan EFCore PrimaryKey olmadıgı için hata vermesin diye EFCore PrimaryKey i olmadıgını bildiriyorum.
+            modelBuilder.Entity<ProductEssential>().ToSqlQuery("Select Name,Price From Products");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
