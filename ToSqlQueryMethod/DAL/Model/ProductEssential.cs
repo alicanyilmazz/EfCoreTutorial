a2badbea6dba3227a7baaa4d62a0143a4b3a43fb
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-17 22:09:19 +0300

    ProductEssential added.

---
+++ ToSqlQueryMethod/DAL/Model/ProductEssential.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToSqlQueryMethod.DAL.Model
+{
+    public class ProductEssential
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
