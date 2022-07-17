53b6537c765b6d09c8c0987269acd21fe8f9df1e
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-17 22:02:12 +0300

    Person added.

---
+++ Query.I/DAL/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.I.DAL
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
