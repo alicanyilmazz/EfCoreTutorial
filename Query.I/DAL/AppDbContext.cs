37ed66e5240a34ce4df92d022e61ad6e4992a32f
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-17 22:01:54 +0300

    AppDbContext added.

---
+++ Query.I/DAL/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query.I.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
