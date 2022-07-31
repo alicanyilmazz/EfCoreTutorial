dc412c448627355259487cbdba89fafdd38aa9cd
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-31 12:14:50 +0300

    ProductWhole added.

---
+++ ToViewMethod/Model/ProductWhole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToViewMethod.Model
+{
+    public class ProductWhole
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
