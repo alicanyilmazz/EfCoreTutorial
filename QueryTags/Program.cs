5e0ed9843b416484455a7946cd45c1f3a3672067
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-31 12:08:48 +0300

    program.cs created.

---
+++ QueryTags/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QueryTags.DAL;
+
+using (var context = new AppDbContext())
+{
+    var productsWithFeatures = context.Products.TagWith(SQLQUERYMESSAGES.GettingProductsWithProductFeatures).Include(x => x.ProductFeature).Where(x=>x.Price > 30).ToList();
+
+    Console.WriteLine("");
+}
+
+class SQLQUERYMESSAGES
+{
+    public static string GettingProductsWithProductFeatures = "This Query Gets Products with ProductFeature.";
+}
