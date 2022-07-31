51da8987b0fcdda7b76cb6606fe625c32d8745c9
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-31 12:13:20 +0300

    Product created.

---
+++ StoreProcedured.IV/DAL/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreProcedured.IV.DAL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal tax { get; set; }
+        public int Stock { get; set; }
+        public int Barcode { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ProductFeature ProductFeature { get; set; }
+    }
+}
