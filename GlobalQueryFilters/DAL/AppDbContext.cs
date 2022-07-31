e12f8d8c7648bd9a384d61c0232abc1f70019563
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-31 12:03:30 +0300

    AppDbContext created.

---
+++ GlobalQueryFilters/DAL/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalQueryFilters.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeature { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Initializer.Build();
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information).UseSqlServer(Initializer.Configuration.GetConnectionString("SqlConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(x => x.Products).WithOne(x => x.Category).HasForeignKey(x => x.CategoryId);
+            modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id);
+
+            //Product tablosundaki "IsDeleted" Column ın default olarak "False" degerini set ettik.
+            modelBuilder.Entity<Product>().Property(x => x.IsDeleted).HasDefaultValue(false);
+
+
+            modelBuilder.Entity<Product>().HasQueryFilter(p => !p.IsDeleted); // Aşağıdaki ile birebir aynıdır.
+            //modelBuilder.Entity<Product>().HasQueryFilter(p => p.IsDeleted == false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
