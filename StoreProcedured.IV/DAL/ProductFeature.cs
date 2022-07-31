71856a0c541829f3be4bc98e1301711b35216d7a
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-07-31 12:13:31 +0300

    ProductFeature created.

---
+++ StoreProcedured.IV/DAL/ProductFeature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreProcedured.IV.DAL
+{
+    public class ProductFeature
+    {
+        public int Id { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Color { get; set; }
+        public Product Product { get; set; }
+    }
+}
